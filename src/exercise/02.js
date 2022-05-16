f5ff493a2f8c0909fc033ddcbfa670b0e3de2f07
Nick Ang <nickangtc@gmail.com>
2022-05-16 14:15:00 +0200

    02 custom hook done

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -3,14 +3,19 @@
 
 import * as React from 'react'
 
-function Greeting({initialName = ''}) {
-  // 🐨 initialize the state to the value from localStorage
-  // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+function useSyncLocalStorageState(key, defaultValue = '') {
+  const [state, setState] = React.useState(
+    () => window.localStorage.getItem(key) ?? defaultValue,
+  )
 
-  // 🐨 Here's where you'll use `React.useEffect`.
-  // The callback should set the `name` in localStorage.
-  // 💰 window.localStorage.setItem('name', name)
+  React.useEffect(() => {
+    window.localStorage.setItem(key, state)
+  })
+  return [state, setState]
+}
+
+function Greeting({initialName = 'John Doe'}) {
+  const [name, setName] = useSyncLocalStorageState('name', initialName)
 
   function handleChange(event) {
     setName(event.target.value)
